94dfbefd86ea3ecc6e9cc63693e4f1c1dea0bb53
danielle <danielle@stackery.io>
2020-01-08 14:12:42 -0800

    Day 4 part 2.

---
+++ 4/part2.js
@@ -0,0 +1,51 @@
+const input = '278384-824795';
+const start = input.split('-')[0];
+const end = input.split('-')[1];
+let count = 0;
+
+for (let i = start; i <= end; i++) {
+  console.log(i);
+  let twoAdjacentDigitsSame = false;
+  let numberDecreased = false;
+  const digits = i.toString().split('').map(num => Number(num));
+
+  if (digits.length !== 6) {
+    continue;
+  }
+
+  for (let j = 0; j < digits.length - 1; j++) {
+    const firstNum = digits[j];
+    const secondNum = digits[j + 1];
+
+    if (firstNum === secondNum) {
+      let matchesBefore = false;
+      if (j !== 0) {
+        if (firstNum === digits[j - 1]) {
+          matchesBefore = true;
+        }
+      }
+
+      let matchesAfter = false;
+      if (j !== 4) {
+        if (secondNum === digits[j + 2]) {
+          matchesAfter = true;
+        }
+      }
+
+      if (!matchesBefore && !matchesAfter) {
+        twoAdjacentDigitsSame = true;
+      }
+    }
+
+    if (firstNum > secondNum) {
+      numberDecreased = true;
+      break;
+    }
+  }
+
+  if (twoAdjacentDigitsSame && !numberDecreased) {
+    count++;
+  }
+}
+
+console.log(`Final count of pw combos that meet the criteria: ${count}`);
