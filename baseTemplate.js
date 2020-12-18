b5e7f57685e0da558c7607f4f09005d1257238fa
danielle <danielle@stackery.io>
2020-12-17 17:40:10 -0800

    Update base template

--- baseTemplate.js
+++ baseTemplate.js
@@ -1,18 +1,13 @@
-// Just a starter base to begin each challenge
 const { promisify } = require('util');
 const fs = require('fs');
 const readFile = promisify(fs.readFile);
 
 (async function () {
-  try {
-    const rawInput = await readFile('input.txt', 'utf-8');
-    const input = rawInput.split('\n');
-    let answer;
+  const rawInput = await readFile('input.txt', 'utf-8');
+  const input = rawInput.split('\n');
+  let answer;
 
-    // answer here
+  // answer here
 
-    console.log(`Answer is ${answer}`);
-  } catch (err) {
-    console.error(err.message);
-  }
+  console.log(`Answer is ${answer}`);
 })();
