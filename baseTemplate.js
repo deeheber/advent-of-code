7a70817c6f5c4d5d8083e33dd27061133046ab34
Danielle Heberling <daheber@gmail.com>
2021-01-09 10:23:46 -0800

    update base template

--- baseTemplate.js
+++ baseTemplate.js
@@ -1,9 +1,7 @@
-const { promisify } = require('util');
-const fs = require('fs');
-const readFile = promisify(fs.readFile);
+const fs = require('fs/promises');
 
 (async function () {
-  const rawInput = await readFile('input.txt', 'utf-8');
+  const rawInput = await fs.readFile('input.txt', 'utf-8');
   const input = rawInput.split('\n');
   let answer;
 
