8db83352e6d0d8f2a1c6dfccd0d94e7515f8875b
deeheber <daheber@gmail.com>
2021-01-18 12:41:24 -0800

    refactor 2019 day 2 part 2

--- 2019/day-2/part2.js
+++ 2019/day-2/part2.js
@@ -1,63 +1,48 @@
-const { promisify } = require('util');
 const fs = require('fs');
-const readFilePromise = promisify(fs.readFile);
+const rawInput = fs.readFileSync('input.txt', 'utf-8');
 
-async function main () {
-  // Generate all possible combinations
-  const combinations = [];
-
-  for (let i = 0; i < 100; i++) {
-    for (let j = 0; j < 100; j++) {
-      combinations.push([i, j]);
-    }
-  }
-
-  // Cycle through each noun/verb combo until address 0 has a value of 19690720
-  for (let index = 0; index < combinations.length; index++) {
-    const noun = combinations[index][0];
-    const verb = combinations[index][1];
-
-    try {
-      const document = await readFilePromise('input.txt', 'utf-8');
-      const values = document.split(',').map(number => Number(number));
-
-      values[1] = noun;
-      values[2] = verb;
-
-      let startIndex = 0;
-      let endIndex = 3;
-
-      while (values[endIndex] !== undefined && values[startIndex] !== 99) {
-        const opicode = values[startIndex];
-        const val1Index = values[startIndex + 1];
-        const val2Index = values[startIndex + 2];
-        let result;
-
-        if (opicode === 1) {
-          result = values[val1Index] + values[val2Index];
-        } else if (opicode === 2) {
-          result = values[val1Index] * values[val2Index];
-        } else {
-          throw new Error(`Invalid opicode value: ${opicode}`);
-        }
-
-        const outputIndex = values[endIndex];
-        values[outputIndex] = result;
-
-        // Move to new position
-        startIndex += 4;
-        endIndex += 4;
-      }
-
-      if (values[0] === 19690720) {
-        console.log(`Noun is ${noun}`);
-        console.log(`Verb is ${verb}`);
-        return;
-      }
-    } catch (err) {
-      console.error(err.message);
-    }
+// Generate all possible combos
+const combinations = [];
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    combinations.push([i, j]);
   }
 }
 
-main();
+// Each combo add noun in position 1 and verb in position 2
+// Keep trying until position 0 is 19690720
+for (let j = 0; j < combinations.length; j++) {
+  // Reset the input on each combo try
+  const input = rawInput.split(',').map(num => Number(num));
+  const [noun, verb] = combinations[j];
+
+  input[1] = noun;
+  input[2] = verb;
+  
+  // Update the input per the opicode rules
+  for (let i = 0; i < input.length; i += 4) {
+    const opicode = input[i];
+    const input1 = input[i + 1];
+    const input2 = input[i + 2];
+    const ouptputIndex = input[i + 3];
+    let result;
+  
+    if (opicode === 1) {
+      result = input[input1] + input[input2];
+    } else if (opicode === 2) {
+      result = input[input1] * input[input2];
+    } else if (opicode === 99) {
+      break;
+    } else {
+      throw new Error(`Invalid opicode: ${opicode}`);
+    }
+  
+    input[ouptputIndex] = result;
+
+    if (input[0] === 19690720) {
+      console.log(`Answer is ${100 * noun + verb}`);
+      // 7610
+      return;
+    }
+  }
+}
