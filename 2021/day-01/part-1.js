7d8b2b57d45d52989a3550373ff9c85ccb445b76
Danielle Heberling <deeheber@users.noreply.github.com>
2021-12-20 18:01:35 -0800

    2021: day1 part 1

--- 2021/day-01/part-1.js
+++ 2021/day-01/part-1.js
@@ -2,11 +2,27 @@ import fs from 'fs/promises';
 
 try {
   const rawInput = await fs.readFile('day-01/input.txt', 'utf-8');
-  const items = rawInput.split('\n');
+  const depths = rawInput.split('\n').map(x => Number(x));
 
-  for (const item of items) {
-    console.log(item);
+  let previous;
+  let increases = 0;
+
+  for (const depth of depths) {
+    // Skip the first item
+    if (!previous) {
+      previous = depth;
+      continue;
+    }
+
+    // Did it increase???
+    if (depth > previous) {
+      increases += 1;
+    }
+
+    previous = depth;
   }
+
+  console.log(`Answer is: ${increases}`)
 } catch (err) {
   console.error(`There was an error: ${err}`);
 }
