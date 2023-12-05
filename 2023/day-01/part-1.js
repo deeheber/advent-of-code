6014439e486ccad5b8053b40d06aff96c6e38c61
Danielle Heberling <deeheber@users.noreply.github.com>
2023-12-05 12:57:02 -0800

    2023: Day 1 Part 1

--- 2023/day-01/part-1.js
+++ 2023/day-01/part-1.js
@@ -2,11 +2,24 @@ import { readFile } from 'fs/promises'
 
 try {
   const rawInput = await readFile('day-01/input.txt', 'utf-8')
-  const items = rawInput.split('\n')
+  const lines = rawInput.split('\n')
+  const calibrationValues = []
 
-  for (const item of items) {
-    console.log(item)
+  for (const line of lines) {
+    let nums = []
+    for (const char of line) {
+      if (!isNaN(char)) {
+        // It is a number
+        nums.push(char)
+      }
+    }
+
+    const lineCalibrationValue = nums.at(0) + nums.at(nums.length - 1)
+    calibrationValues.push(Number(lineCalibrationValue))
   }
+
+  console.log(calibrationValues.reduce((a, b) => a + b, 0))
+  // 55029
 } catch (err) {
   console.error(`There was an error: ${err}`)
 }
