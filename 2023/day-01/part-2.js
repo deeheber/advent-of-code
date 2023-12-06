0eb6794a775f10a91d65ef4773209ffb577375c0
Danielle Heberling <deeheber@users.noreply.github.com>
2023-12-05 18:00:36 -0800

    2023: Day 1 Part 2

--- 2023/day-01/part-2.js
+++ 2023/day-01/part-2.js
@@ -2,11 +2,47 @@ import { readFile } from 'fs/promises'
 
 try {
   const rawInput = await readFile('day-01/input.txt', 'utf-8')
-  const items = rawInput.split('\n')
+  const lines = rawInput.split('\n')
+  const calibrationValues = []
 
-  for (const item of items) {
-    console.log(item)
+  for (const line of lines) {
+    let nums = []
+
+    for (let i = 0; i < line.length; i++) {
+      if (!isNaN(line[i])) {
+        // It is a digit number
+        nums.push(line[i])
+      }
+
+      // Check for spelled out nums
+      const spelledOutNums = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine'
+      ]
+      const substring = line.substring(i)
+      for (let j = 0; j < spelledOutNums.length; j++) {
+        const currNum = spelledOutNums[j]
+        if (substring.startsWith(currNum)) {
+          // Zero indexed + 1 to get the digit
+          nums.push(j + 1)
+          break
+        }
+      }
+    }
+
+    const lineCalibrationValue = `${nums.at(0)}${nums.at(nums.length - 1)}`
+    calibrationValues.push(Number(lineCalibrationValue))
   }
+
+  console.log(calibrationValues.reduce((a, b) => a + b, 0))
+  // 55686
 } catch (err) {
   console.error(`There was an error: ${err}`)
 }
