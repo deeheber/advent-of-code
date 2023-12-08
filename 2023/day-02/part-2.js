568de9da7d4f213fef86b1af2810719f0eacac6d
Danielle Heberling <deeheber@users.noreply.github.com>
2023-12-07 19:41:03 -0800

    2023: Day 2 Part 2

--- 2023/day-02/part-2.js
+++ 2023/day-02/part-2.js
@@ -3,10 +3,34 @@ import { readFile } from 'fs/promises'
 try {
   const rawInput = await readFile('day-02/input.txt', 'utf-8')
   const lines = rawInput.split('\n')
+  let answer = 0
 
-  for (const line of lines) {
-    console.log(line)
+  for (let i = 0; i < lines.length; i++) {
+    const rounds = lines[i].split(': ')[1]
+    const cubes = rounds.split('; ').map((round) => round.split(', '))
+
+    const max = {
+      green: 0,
+      red: 0,
+      blue: 0
+    }
+
+    for (let j = 0; j < cubes.length; j++) {
+      for (let k = 0; k < cubes[j].length; k++) {
+        const [num, color] = cubes[j][k].split(' ')
+
+        if (max[color] < Number(num)) {
+          max[color] = Number(num)
+        }
+      }
+    }
+
+    const cube = max['green'] * max['red'] * max['blue']
+    answer += cube
   }
+
+  console.log(`Answer is: ${answer}`)
+  // 65371
 } catch (err) {
   console.error(`There was an error: ${err}`)
 }
