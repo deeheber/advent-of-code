72aa164bbeb9d200ef82fee91d927a1b7f2c2519
Danielle Heberling <deeheber@users.noreply.github.com>
2025-09-29 16:29:43 -0700

    2024: Day 1/Part 1

--- 2024/day-01/part-1.js
+++ 2024/day-01/part-1.js
@@ -4,9 +4,28 @@ try {
   const rawInput = await readFile('day-01/input.txt', 'utf-8')
   const items = rawInput.split('\n')
 
+  const left = []
+  const right = []
+
   for (const item of items) {
-    console.log(item)
+    // TODO adjust split size ???
+    const [first, second] = item.split('   ')
+    left.push(Number(first))
+    right.push(Number(second))
   }
+
+  left.sort((a, b) => a - b)
+  right.sort((a, b) => a - b)
+
+  let result = 0
+  for (let i = 0; i < left.length; i++) {
+    const first = left[i]
+    const second = right[i]
+
+    result += Math.abs(first - second)
+  }
+  // 1580061
+  console.log(`Result is ${result}`)
 } catch (err) {
   console.error(`There was an error: ${err}`)
 }
