aa71c85e495860d129b5f8449f48319d3a0f789c
Danielle Heberling <deeheber@users.noreply.github.com>
2025-12-11 16:11:01 -0800

    2025: Day 1/Part 2

---
+++ 2025/day-01/part-2.js
@@ -0,0 +1,43 @@
+import { readFile } from 'fs/promises'
+
+try {
+  const rawInput = await readFile('day-01/input.txt', 'utf-8')
+  const rotations = rawInput.split('\n')
+
+  let answer = 0
+  let location = 50
+  for (const rotation of rotations) {
+    const direction = rotation[0]
+    let amount = parseInt(rotation.slice(1))
+    // L -
+    // R +
+    while (amount > 0) {
+      amount--
+      // Move left or right one
+      if (direction === 'L') {
+        location--
+      } else {
+        // R
+        location++
+      }
+
+      // Reset if overflow
+      if (location > 99) {
+        location = 0
+      }
+      if (location < 0) {
+        location = 99
+      }
+
+      if (location === 0) {
+        answer++
+      }
+    }
+
+    console.debug(`Location is ${location}`)
+  }
+  // 5892
+  console.log(`Answer is: ${answer}`)
+} catch (err) {
+  console.error(`There was an error: ${err}`)
+}
